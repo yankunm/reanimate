beb71b1ea2ea76992dc359faf54554d3022560c3
luojeffery <jeffery.luo@stonybrook.edu>
2022-09-25 06:06:19 -0400

    cross origin error fixed, too late to actually implement js-aruco in full

--- webapp/js/demo.js
+++ webapp/js/demo.js
@@ -1,7 +1,14 @@
-let demo;
+$(document).ready(function() {
+    $("#livestream").hide();
+    //$(".demo-vid").hide();
+});
 
 $("#submit").on("click", function() {
     $(".date").hide();
     $(".time").hide();
+    $("#livestream").show();
+    //$(".demo-vid").show();
     new p5(s);
+
+
 });
--- webapp/js/sketch.js
+++ webapp/js/sketch.js
@@ -1,25 +1,47 @@
-
 const s = (sketch) => {
-  var capture;
-  var url = "http://10.1.135.184:8080/video";
-  let cv = window.cv;
-
+  var detector, context;
   sketch.setup = () => {
     width = $("#screen-id")[0].clientWidth;
     height = $("#screen-id")[0].clientHeight;
-    console.log(width);
     var canvas = sketch.createCanvas(width, height);
     canvas.parent("screen-id");
     sketch.background(sketch.color("white"));
-
-    capture = new cv.VideoCapture(url);
+    var canv = $("#canvas");
+    context = canv.getContext("2d");
+    detector = new AR.Detector();
   };
   
   sketch.draw = () => {
-    [camera, frame] = capture.read();
-
-    if (frame !== null)
-      cv.imshow("Frame", frame);
+    img = cv.imread("livestream");
+    var markers = detector.detect(img);
+    drawCorners(markers);
   };
-}
 
+         
+  function drawCorners(markers){
+    var corners, corner, i, j;
+  
+    context.lineWidth = 3;
+
+    for (i = 0; i !== markers.length; ++ i){
+      corners = markers[i].corners;
+      
+      context.strokeStyle = "red";
+      context.beginPath();
+      
+      for (j = 0; j !== corners.length; ++ j){
+        corner = corners[j];
+        context.moveTo(corner.x, corner.y);
+        corner = corners[(j + 1) % corners.length];
+        context.lineTo(corner.x, corner.y);
+      }
+
+      context.stroke();
+      context.closePath();
+      
+      context.strokeStyle = "green";
+      context.strokeRect(corners[0].x - 2, corners[0].y - 2, 4, 4);
+    }
+  }
+}
+ 
